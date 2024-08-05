c1a50ac041832919651745d406fca6c7dc806831
ana.vazquez.orihuela <ana.vazquez.orihuela@gmail.com>
2024-08-05 13:47:26 -0600

    Activities Session 01

---
+++ apps/vue-app/src/app/modules/categories/views/CategoryView.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="row pt-5">
+    <CategoryList />
+    <CategoryForm />
+</div>
+</template>
+
+<script>
+import CategoryForm from '../components/CategoryForm.vue';
+import CategoryList from '../components/CategoryList.vue';
+
+export default {
+    components: {
+        CategoryForm,
+        CategoryList
+    },
+    data() {
+        return {};
+    }
+};
+</script>
