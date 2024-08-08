08712f5fd361f64e24d8a1ad3bbbf9a26acd9563
Darien Ramirez <darien.ramirez@accenture.com>
2024-08-08 12:19:15 -0500

    Session 1

---
+++ apps/vue-app/src/app/modules/categories/components/CategoryList.vue
@@ -0,0 +1,48 @@
+<template lang="">
+  <div class="container mt-5">
+    <div class="d-flex justify-content-between">
+      <h1 class="display-6">Categories</h1>
+      <button class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#createCategoryModal">Add Category</button>
+    </div>
+
+    <hr />
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Category name</td>
+          <td>
+            <i class="fa-solid fa-pen me-3" data-bs-toggle="modal" data-bs-target="#createCategoryModal"></i>
+            <i class="fa-solid fa-trash">
+
+            </i>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <CategoryForm />
+  </div>
+</template>
+
+<script>
+import CategoryForm from '../components/CategoryForm.vue'
+
+export default {
+  name: 'CategoryList',
+  methods: {},
+  components: {
+    CategoryForm
+  }
+}
+</script>
+<style lang="">
+
+</style>
